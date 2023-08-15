23fd05f0efe51e2e2da795e6999381394b80e25b
Mirassik <221675@astanait.edu.kz>
2023-08-15 13:24:22 +0600

    Auth by clerk added as middleware

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1>helo</h1>
-  )
-}
---
+++ src/components/providers.tsx
@@ -0,0 +1,10 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+
+export default function Providers({children}: {children: React.ReactNode}) {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
